9babc700ace1b30348c9b6630cc9cf828dd96c1a
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 18:43:09 +0200

    Customize response in book ms

--- rest-book/src/main/java/org/thywill/quarkus/microservices/number/Book.java
+++ rest-book/src/main/java/org/thywill/quarkus/microservices/number/Book.java
@@ -1,13 +1,22 @@
 package org.thywill.quarkus.microservices.number;
 
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import jakarta.json.bind.annotation.JsonbProperty;
+
 import java.time.Instant;
 
 public class Book {
+    @JsonbProperty("isbn_13")
     public String isbn13;
     public String title;
     public String author;
+
+    @JsonbProperty("year_of_publication")
     public int yearOfPublication;
     public String genre;
+
+    @JsonbDateFormat("yyyy/MM/dd")
+    @JsonbProperty("creation_date")
     public Instant creationDate;
 
     @Override
