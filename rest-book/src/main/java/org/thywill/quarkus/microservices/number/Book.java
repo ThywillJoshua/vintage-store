f3bb21835f68880b31c84650d392027cfbc99a66
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 18:58:02 +0200

    Add documentations in book ms

--- rest-book/src/main/java/org/thywill/quarkus/microservices/number/Book.java
+++ rest-book/src/main/java/org/thywill/quarkus/microservices/number/Book.java
@@ -2,21 +2,32 @@ package org.thywill.quarkus.microservices.number;
 
 import jakarta.json.bind.annotation.JsonbDateFormat;
 import jakarta.json.bind.annotation.JsonbProperty;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.time.Instant;
 
+@Schema(description = "This is a book")
 public class Book {
+    @Schema(required = true)
     @JsonbProperty("isbn_13")
     public String isbn13;
+
+    @Schema(required = true)
     public String title;
+
+    @Schema(required = true)
     public String author;
 
+    @Schema(required = true)
     @JsonbProperty("year_of_publication")
     public int yearOfPublication;
+
+    @Schema(required = true)
     public String genre;
 
     @JsonbDateFormat("yyyy/MM/dd")
     @JsonbProperty("creation_date")
+    @Schema(implementation = String.class, format = "date", required = true)
     public Instant creationDate;
 
     @Override
--- rest-book/src/main/java/org/thywill/quarkus/microservices/number/BookResource.java
+++ rest-book/src/main/java/org/thywill/quarkus/microservices/number/BookResource.java
@@ -4,25 +4,25 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
 
 @Path("/api/books")
+@Tag(name = "Book REST endpoint")
 public class BookResource {
 
     @Inject
     Logger logger;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getABook() {
-        return "Hello RESTEasy";
-    }
-
    @POST
    @Produces(MediaType.APPLICATION_JSON)
    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+   @Operation(
+           summary = "Creates a Book",
+           description = "Creates a Book with an ISBN number")
     public Response createABook (
             @FormParam("title") String title,
             @FormParam("author") String author,
