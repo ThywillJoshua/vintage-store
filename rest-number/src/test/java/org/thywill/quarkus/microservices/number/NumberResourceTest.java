4a47fb989b9ada3e9ac98f28631d31e2154e14b0
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 18:13:29 +0200

    Add tests to numbers ms

--- rest-number/src/test/java/org/thywill/quarkus/microservices/number/NumberResourceTest.java
+++ rest-number/src/test/java/org/thywill/quarkus/microservices/number/NumberResourceTest.java
@@ -4,17 +4,19 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 class NumberResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void shouldGenerateIsbnNumbers() {
         given()
           .when().get("/api/numbers")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(not(hasItem("generationDate")));
     }
 
 }
