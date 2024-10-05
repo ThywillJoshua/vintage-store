17b06a1b77de3036035601e71b9a96ba48af38eb
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 16:01:52 +0200

    exposing the numbers api and creating numbers clas

--- rest-number/src/main/java/org/thywill/quarkus/microservices/number/NumberResource.java
+++ rest-number/src/main/java/org/thywill/quarkus/microservices/number/NumberResource.java
@@ -1,16 +1,30 @@
 package org.thywill.quarkus.microservices.number;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
+import java.util.Random;
 
 @Path("/api/numbers")
 public class NumberResource {
 
+    @Inject
+    Logger logger;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    public IsbnNumbers IsbnNumbers() {
+        IsbnNumbers isbnNumbers = new IsbnNumbers();
+        isbnNumbers.isbn13 = "13-" + new Random().nextInt(100_000_000);
+        isbnNumbers.isbn10 = "10-" + new Random().nextInt(100_000);
+        isbnNumbers.generationDate = Instant.now();
+
+        logger.info("Numbers generated: " + isbnNumbers);
+        return isbnNumbers;
     }
 }
