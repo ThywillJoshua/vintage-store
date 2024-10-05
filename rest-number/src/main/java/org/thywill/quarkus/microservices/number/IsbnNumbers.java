adac3b8a398a1f192570cfa2e1dd6dc92bb31588
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 17:37:30 +0200

    Customize response with JSONB

--- rest-number/src/main/java/org/thywill/quarkus/microservices/number/IsbnNumbers.java
+++ rest-number/src/main/java/org/thywill/quarkus/microservices/number/IsbnNumbers.java
@@ -1,10 +1,18 @@
 package org.thywill.quarkus.microservices.number;
 
+import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.json.bind.annotation.JsonbTransient;
+
 import java.time.Instant;
 
 public class IsbnNumbers {
+    @JsonbProperty("isbn_13")
     public String isbn13;
+
+    @JsonbProperty("isbn_10")
     public String isbn10;
+
+    @JsonbTransient
     public Instant generationDate;
 
     @Override
