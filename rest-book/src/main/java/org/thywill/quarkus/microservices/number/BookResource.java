0ccdab497f7bcb2d039fcae38bcf149cd05878b8
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 23:52:20 +0200

    Add number ms proxy in the book ms

--- rest-book/src/main/java/org/thywill/quarkus/microservices/number/BookResource.java
+++ rest-book/src/main/java/org/thywill/quarkus/microservices/number/BookResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
@@ -17,26 +18,31 @@ public class BookResource {
     @Inject
     Logger logger;
 
-   @POST
-   @Produces(MediaType.APPLICATION_JSON)
-   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-   @Operation(
-           summary = "Creates a Book",
-           description = "Creates a Book with an ISBN number")
-    public Response createABook (
+    @Inject
+    @RestClient
+    NumberProxy numberProxy;
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary = "Creates a Book",
+            description = "Creates a Book with an ISBN number")
+    public Response createABook(
             @FormParam("title") String title,
             @FormParam("author") String author,
             @FormParam("yearOfPublication") int yearOfPublication,
             @FormParam("genre") String genre) {
-    Book book = new Book();
-    book.isbn13 = "13- To be implemented";
-    book.title = title;
-    book.author = author;
-    book.yearOfPublication = yearOfPublication;
-    book.genre = genre;
-    book.creationDate = Instant.now();
 
-    logger.info("Created a book: " + book);
-    return Response.status(201).entity(book).build();
-   }
+        Book book = new Book();
+        book.isbn13 = numberProxy.generateIsbnNumbers().isbn_13;
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+
+        logger.info("Created a book: " + book);
+        return Response.status(201).entity(book).build();
+    }
 }
---
+++ rest-book/src/main/java/org/thywill/quarkus/microservices/number/IsbnThirteen.java
@@ -0,0 +1,7 @@
+package org.thywill.quarkus.microservices.number;
+
+import jakarta.json.bind.annotation.JsonbProperty;
+
+public class IsbnThirteen {
+    public String isbn_13;
+}
